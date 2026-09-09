370d17d7ae902e8324585e00c047ecf6e92447a0
Rashel Ali <rashelali347632@gmail.com>
2026-09-09 12:48:09 +0600

    Update app Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,34 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mymusicapp"
+    namespace = "com.example.onlinemusic"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.mymusicapp"
+        applicationId = "com.example.onlinemusic"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui:1.7.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
+    implementation("androidx.compose.material3:material3:1.3.1")
+
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-ui:1.5.1")
 }
